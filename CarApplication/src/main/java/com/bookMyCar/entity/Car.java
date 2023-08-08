22d13146ae351e3a5828b780480a6888f90293ab
HarshaBO <harshavardhanbo17@gmail.com>
2023-08-08 22:05:47 +0530

    Updated entity class

--- CarApplication/src/main/java/com/bookMyCar/entity/Car.java
+++ CarApplication/src/main/java/com/bookMyCar/entity/Car.java
@@ -7,4 +7,19 @@ import jakarta.persistence.Id;
 public class Car {
 	@Id
 	private Integer id;
+	private String name;
+	public Integer getId() {
+		return id;
+	}
+	public void setId(Integer id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	
 }
